d58d96018df1ae7d0b1ad5b20a111269ea8dcb66
Farj980627 <chuma--00@hotmail.com>
2017-11-06 20:04:26 -0600

    Pagina piloto

---
+++ Lawyer/Index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/mycss.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="js/myjs.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+
+<div class="backgroundtop">
+<!--    LOGO PRINCIPAL -->
+        <a href="index.php" class="brand-logo">  <img class="responsive-img" src="img/logo.png"></a>
+<!--    LOGO PRINCIPAL -->
+
+<!--    BARRA DE NAVEGACION -->
+        <nav class="margin-nav">
+        <div class="nav-wrapper pink darken-2">
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li class="menu-element"><a href="index.php">Inicio</a></li>
+            <li class= "spacer2"></li>
+            <li class="menu-element"><a href="html/Contact.php">Contacto</a></li>
+            <li class= "spacer2"></li>
+            <li class="menu-element"><a href="html/About.html">Nosotros</a></li>
+            <li class= "spacer1"></li>
+          </ul>
+        </div>
+        </nav>
+<!--    BARRA DE NAVEGACION -->
+
+
+<!--   TEXTO INTRO PEQUEÑO -->
+        <nav class="square-name">
+          M&M
+        </nav>
+<!--   TEXTO INTRO GRANDE -->
+
+<!--   TEXTO INTRO PEQUEÑO -->
+        <nav class="square-sub">
+          Nuestro método de trabajo garantiza un trabajo profesional en tiempo record.
+        </nav>
+<!--   TEXTO INTRO PEQUEÑO -->
+</div>
+
+<div class="backgroundmid">
+<div class="spacer"></div>
+<!--   SLIDER -->
+        <div class="slider">
+        <ul class="slides">
+          <li>
+            <img src="img/1.jpg"> <!-- random image -->
+            <div class="caption center-align">
+              <h3>PRESENTACIÓN DEL DESPACHO</h3>
+              <h5 class="light grey-text text-lighten-3">Presentación del despacho, su origen, objetivos y sus principales ventajas competitivas.</h5>
+            </div>
+          </li>
+          <li>
+            <img src="img/2.jpg"> <!-- random image -->
+            <div class="caption left-align">
+              <h3>MIEMBROS DEL DESPACHO</h3>
+              <h5 class="light grey-text text-lighten-3">Presentación de los miembros del equipo, perfil profesional y áreas de trabajo.</h5>
+            </div>
+          </li>
+          <li>
+            <img src="img/3.jpg"> <!-- random image -->
+            <div class="caption right-align">
+              <h3>METODOLOGÍA DE TRABAJO</h3>
+              <h5 class="light grey-text text-lighten-3">Descripción de la metodología de trabajo, el enfoque y los procesos que definen la actividad del despacho.</h5>
+            </div>
+          </li>
+          <li>
+            <img src="img/4.jpg"> <!-- random image -->
+            <div class="caption center-align">
+              <h3>LOCALIZACIÓN Y CONTACTO</h3>
+              <h5 class="light grey-text text-lighten-3">Área de contacto, recogida de consultas mediante formulario y plano de localización.</h5>
+            </div>
+          </li>
+        </ul>
+       </div>
+<!--   SLIDER -->
+</div>
+
+<!--    COLUMNAS DE INFORMACION -->
+<div class="backgroundbot">
+
+       <div class="info">
+         <div class="col1">
+            <img src="img/p1.png" alt="" class="circle responsive-img">
+
+            <div class="col-title">
+            <b>Lorem ipsum dolor sit amet</b>
+            </div>
+            <div class="col-text">
+              Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            </div>
+         </div>
+         <div class="col1">
+            <img src="img/p2.png" alt="" class="circle responsive-img">
+            <div class="col-title">
+                <b>Sed ut perspiciatis unde</b>
+            </div>
+            <div class="col-text">
+              Voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.
+            </div>
+         </div>
+         <div class="col1">
+            <img src="img/p3.png" alt="" class="circle responsive-img">
+            <div class="col-title">
+               <b> At vero eos et accusamus</b>
+            </div>
+            <div class="col-text">
+              Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+            </div>
+         </div>
+       </div>
+</div>
+<!--    COLUMNAS DE INFORMACION -->
+
+<!-- FOOTER -->
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+               <div class="col l6 s12">
+                  <h5 class="white-text">Informacion adicional.</h5>
+                  <p class="grey-text text-lighten-4">Voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                </div>
+                 <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Redes sociales</h5>
+                     <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Linkedin</a></li>
+                     </ul>
+                 </div>
+              </div>
+            </div>
+            <div class="footer-copyright">
+              <div class="container">
+                © 2017 Advocatorum M&M
+              </div>
+            </div>
+        </footer>
+<!-- FOOTER -->
+
+    </body>
+  </html>
